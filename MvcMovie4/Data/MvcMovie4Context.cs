2e2b5aa2c964e54bbcf0ad99d9f1d3392e73a2d8
Ankichi <59509443+An39nA0219@users.noreply.github.com>
2021-10-25 13:42:25 +0900

    モデルの追加

---
+++ MvcMovie4/Data/MvcMovie4Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MvcMovie4.Models;
+
+namespace MvcMovie4.Data
+{
+    public class MvcMovie4Context : DbContext
+    {
+        public MvcMovie4Context (DbContextOptions<MvcMovie4Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MvcMovie4.Models.Movie> Movie { get; set; }
+    }
+}
